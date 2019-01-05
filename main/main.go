c92d619e9341320cfe161430a81eba6eb735fb24
d27sa <9d27sa@gmail.com>
2019-01-05 20:23:55 +0800

    1

--- main/main.go
+++ main/main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates, _ := template.ParseFiles("templates/home.html")
+	templates, err := template.ParseFiles("../templates/home.html")
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
 	path := r.URL.Path[1:]
-	templates.ExecuteTemplate(w, "home", fmt.Sprintf("Hello, world!\nThe path is %s.", path))
+	templates.ExecuteTemplate(w, "home.html", fmt.Sprintf("Hello, world!\nThe path is %s.", path))
 }
 
 func main() {
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/", index)
-	http.ListenAndServe(":80", mux)
+	server := http.Server{
+		Addr:    "0.0.0.0:80",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
