7f2a4f6acbb92f88a3070c89f372166345a5f78e
d27sa <9d27sa@gmail.com>
2019-01-06 20:38:51 +0800

    1

--- main/main.go
+++ main/main.go
@@ -3,27 +3,37 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+
+	"github.com/d27sa/www.yuzuka.tk/model"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	templates, err := template.ParseFiles("../templates/home.html")
-	if err != nil {
-		log.Fatal(err.Error())
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("index")
+	templates := template.Must(template.ParseFiles("../templates/layout.html", "../templates/home.html"))
+	path := r.URL.Path[1:]
+	templates.ExecuteTemplate(w, "layout", fmt.Sprintf("Hello, world!\nThe path is %s.", path))
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	templates := template.Must(template.ParseFiles("../templates/layout.html", "../templates/register.html"))
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		user := model.User{Username: username, Email: email, Password: password}
+		templates.ExecuteTemplate(w, "layout", &user)
 		return
 	}
-	path := r.URL.Path[1:]
-	templates.ExecuteTemplate(w, "home.html", fmt.Sprintf("Hello, world!\nThe path is %s.", path))
+	templates.ExecuteTemplate(w, "layout", nil)
 }
 
 func main() {
-	mux := http.DefaultServeMux
-	mux.HandleFunc("/", homeHandler)
-	mux.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("../static/css"))))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("../static/css"))))
 	server := http.Server{
-		Addr:    "0.0.0.0:80",
-		Handler: mux,
+		Addr: ":80",
 	}
 	server.ListenAndServe()
 }
