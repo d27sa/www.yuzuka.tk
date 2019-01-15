cf0ab0b16a022cec5554d3bd9c82575f10dc2bc8
d27sa <9d27sa@gmail.com>
2019-01-15 18:10:54 +0800

    1

--- net/socket/client/main.go
+++ net/socket/client/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
@@ -11,26 +12,28 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		log.Fatal(err)
+	var conn *net.TCPConn
+	resp := make([]byte, 4096)
+	for {
+		conn, err = net.DialTCP("tcp", nil, tcpAddr)
+		if err != nil {
+			log.Println(err)
+			conn.Close()
+			continue
+		}
+		_, err = conn.Write([]byte("Hello! This is a message.\n"))
+		if err != nil {
+			log.Println(err)
+			conn.Close()
+			continue
+		}
+		n, err := conn.Read(resp)
+		if err != nil {
+			log.Println(err)
+			conn.Close()
+			continue
+		}
+		fmt.Println("The server responds: ", string(resp[:n]))
+		time.Sleep(time.Second * 2)
 	}
-
-	fmt.Println("Writing...")
-	_, err = conn.Write([]byte("Hello!\n"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Written.")
-	resp := make([]byte, 1024)
-	n, err := conn.Read(resp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(resp[:n]))
-	_, err = conn.Write([]byte("Hello!\n"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
 }
--- net/socket/server/main.go
+++ net/socket/server/main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 )
+
+func handleTCPConn(conn *net.TCPConn) error {
+	defer conn.Close()
+	data := make([]byte, 4096)
+	for {
+		n, err := conn.Read(data)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		fmt.Println("Received message: ", string(data[:n]))
+		_, err = conn.Write(data[:n])
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+	}
+}
 
 func main() {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", ":8080")
@@ -15,23 +35,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("listening on port 8080 ...")
+	var conn *net.TCPConn
 	for {
-		conn, err := listener.AcceptTCP()
+		conn, err = listener.AcceptTCP()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			continue
 		}
-		data := make([]byte, 1024)
-		for {
-			n, err := conn.Read(data)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(string(data[:n]))
-			_, err = conn.Write(data[:n])
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
+		fmt.Println("New Conn Accepted: ", conn.RemoteAddr())
+		go handleTCPConn(conn)
 	}
 }
