e466d3ee0ff7749b388e7b60ada3853772eab120
d27sa <9d27sa@gmail.com>
2019-01-23 21:58:09 +0800

    1

--- main/mail.go
+++ main/mail.go
@@ -19,7 +19,7 @@ var (
 func mailTest() {
 	auth := smtp.PlainAuth("", localMailAddr, localMailPassword, localMailHost)
 	to := []string{"erciyuangd@163.com"}
-	msg := []byte("To: erciyuangd@163.com\r\n" +
+	msg := []byte("X-Sender: Kousuke\r\nTo: erciyuangd@163.com\r\n" +
 		"Subject: こんにちは！\r\n" +
 		"date: Wed, 23 Jan 2019 20:40:00 +0800\r\n" +
 		"\r\n" +
@@ -34,7 +34,7 @@ func sendCheckMail(addr string) (int, error) {
 	code := rand.Intn(10000)
 	codeStr := strconv.Itoa(code)
 	to := []string{addr}
-	msgStr := fmt.Sprintf("To: %s\r\nSubject: Check code from www.yuzuka.tk\r\n\r\nThe code is %s.\r\n", addr, codeStr)
+	msgStr := fmt.Sprintf("X-Sender: Kousuke\r\nTo: %s\r\nSubject: Check code from www.yuzuka.tk\r\n\r\nThe code is %s.\r\n", addr, codeStr)
 	msg := []byte(msgStr)
 	err := smtp.SendMail(localMailServerAddr, mailAuth, localMailAddr, to, msg)
 	return code, err
