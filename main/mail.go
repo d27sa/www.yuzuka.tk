8a21b818afcdbb7b61c4075b75fede290d065fa3
d27sa <9d27sa@gmail.com>
2019-01-23 20:51:07 +0800

    1

--- main/mail.go
+++ main/mail.go
@@ -6,14 +6,14 @@ import (
 )
 
 func mailTest() {
-	auth := smtp.PlainAuth("", "zkm971227@gmail.com", "zhukm19971227", "smtp.gmail.com")
+	auth := smtp.PlainAuth("", "9d27sa@gmail.com", "zhukm1997", "smtp.gmail.com")
 	to := []string{"erciyuangd@163.com"}
 	msg := []byte("To: erciyuangd@163.com\r\n" +
 		"Subject: こんにちは！\r\n" +
 		"date: Wed, 23 Jan 2019 20:40:00 +0800\r\n" +
 		"\r\n" +
 		"今どこ？\r\n")
-	err := smtp.SendMail("smtp.gmail.com:587", auth, "zkm971227@gmail.com", to, msg)
+	err := smtp.SendMail("smtp.gmail.com:587", auth, "9d27sa@gmail.com", to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}
