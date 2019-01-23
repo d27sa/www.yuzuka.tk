cea1379041c0a83c1ea78bfa3be42fd2b79fbe0c
d27sa <9d27sa@gmail.com>
2019-01-23 20:08:11 +0800

    1

--- main/mail.go
+++ main/mail.go
@@ -12,7 +12,7 @@ func mailTest() {
 		"Subject: discount Gophers!\r\n" +
 		"\r\n" +
 		"This is the email body.\r\n")
-	err := smtp.SendMail("smtp.gmail.com:465", auth, "9d27sa@gmail.com", to, msg)
+	err := smtp.SendMail("smtp.gmail.com:587", auth, "9d27sa@gmail.com", to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}
