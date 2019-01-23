53f67437cc366628ff2edc075f67ee91acc5204f
d27sa <9d27sa@gmail.com>
2019-01-23 21:26:38 +0800

    1

--- main/mail.go
+++ main/mail.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net/smtp"
+	"strconv"
 )
 
 func mailTest() {
-	auth := smtp.PlainAuth("", "9d27sa@gmail.com", "zhukm1997", "smtp.gmail.com")
+	auth := smtp.PlainAuth("", "d27sa@yahoo.co.jp", "zhukm1997", "smtp.mail.yahoo.co.jp")
 	to := []string{"erciyuangd@163.com"}
 	msg := []byte("To: erciyuangd@163.com\r\n" +
 		"Subject: こんにちは！\r\n" +
 		"date: Wed, 23 Jan 2019 20:40:00 +0800\r\n" +
 		"\r\n" +
 		"今どこ？\r\n")
-	err := smtp.SendMail("smtp.gmail.com:587", auth, "9d27sa@gmail.com", to, msg)
+	err := smtp.SendMail("smtp.mail.yahoo.co.jp:587", auth, "d27sa@yahoo.co.jp", to, msg)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func sendCheckMail(addr string) (int, error) {
+	code := rand.Intn(10000)
+	codeStr := strconv.Itoa(code)
+	auth := smtp.PlainAuth("", "d27sa@yahoo.co.jp", "zhukm1997", "smtp.mail.yahoo.co.jp")
+	to := []string{addr}
+	msgStr := fmt.Sprintf("To: %s\r\nSubject: Check code from www.yuzuka.tk\r\n\r\nThe code is %s.\r\n", addr, codeStr)
+	msg := []byte(msgStr)
+	err := smtp.SendMail("smtp.mail.yahoo.co.jp:587", auth, "d27sa@yahoo.co.jp", to, msg)
+	return code, err
 }
